ea02608af88804b8b42c9787ea478a3f32d405b6
vipul-zambare006 <vipulzambare1993@gmail.com>
2017-04-04 00:22:49 +0800

    Created MVC Project

---
+++ Controllers/EventController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AmazingRaceMVC.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AmazingRaceMVC.MvcApplication" Language="C#" %>
