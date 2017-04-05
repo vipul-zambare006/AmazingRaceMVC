f293c39f67ab19bc6aef53a101437db07ef65b81
vipul-zambare006 <vipulzambare1993@gmail.com>
2017-04-05 16:34:18 +0800

    Implemented JqGrid.

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Web.Mvc;
 using BusinessLayer;
-using System.Collections.Generic;
-using AmazingRaceMVC.Models;
+using System.Linq;
 
 namespace AmazingRaceMVC.Controllers
 {
@@ -10,9 +9,36 @@ namespace AmazingRaceMVC.Controllers
         // GET: Event
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult getEvents()
+        {
             EventManagement em = new EventManagement();
-            List<Event> evList = em.GetAllEvents();
-            return View(evList);
+
+            var jsonData = new
+            {
+                total = 1,
+                page = 1,
+                records = em.GetAllEvents().Count,
+
+                rows = (
+                 from ev in em.GetAllEvents().ToList()
+                 select new
+                 {
+                     id = ev.Id,
+                     cell = new string[] {
+                      ev.EventName.ToString(),
+                      ev.EventDateTime.ToString(),
+                      ev.City.ToString(),
+                      }
+                 }).ToArray()
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+            //List<Event> evList = em.GetAllEvents();
+            //return View(evList);
         }
     }
 }
