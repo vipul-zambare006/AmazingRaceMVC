d24bb8ce3d4e4db8c30dd2330274f6e22e490195
prasanna12510 <prasanna.pawar43@gmail.com>
2017-04-25 20:36:09 +0800

    Added Map

---
+++ AmazingRaceMVC/Controllers/MapController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AmazingRaceMVC.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult AmazingMap()
+        {
+            return View();
+        }
+
+
+      
+        
+        public string[] Initialize(string[] initLeaderboard)
+        {
+            if (initLeaderboard != null)
+            {
+                 return initLeaderboard;
+            }
+
+            return null;
+        }
+
+
+        
+
+        public string Execute(string teamObject)
+        {
+            return teamObject;
+        }
+    }
+}
